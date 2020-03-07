4c576f063be70f1b7dbc3d5239ac78bc025f9852
Fabrício Lélis <fabriciolelis@gmail.com>
2020-03-07 14:36:32 -0300

    Updates

---
+++ precache-manifest.ef67d59ff546bc0a26074cbc59bbc9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13bb1b37db7ece1b4d9307cbd9742b4",
+    "url": "/react-study/index.html"
+  },
+  {
+    "revision": "02281b5a85c9eeaf6830",
+    "url": "/react-study/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1014ba326dc0c6b1104d",
+    "url": "/react-study/static/js/2.58bfb1cf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-study/static/js/2.58bfb1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02281b5a85c9eeaf6830",
+    "url": "/react-study/static/js/main.07d2d4a0.chunk.js"
+  },
+  {
+    "revision": "2860d7c505ae2c7d64c1",
+    "url": "/react-study/static/js/runtime-main.ad9ac982.js"
+  }
+]);
